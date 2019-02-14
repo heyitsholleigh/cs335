ffe7397092e0789c555929744c1355d0c2e6739e
Holleigh Landers <hlanders@una.edu>
2019-02-14 15:42:20 +0000

    daily commit

--- objectorient/obj_baby.py
+++ objectorient/obj_baby.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python
 import urllib
+import re
+import sys
+
+class Babynames :
+    def __init__(self, year):
+        self.year = year
 
 
-def wget(url):
+def main():
+    args = sys.argv[1:]
+    if not args:
+       print 'usage: [--summaryfile] URL'
+       sys.exit(1)
+    
+    summary = False
+    if args[0] == '--summaryfile':
+      summary = True
+      del args[0]   
+       
+    
     try:
-        text = urllib.urlopen(url)
+        text = urllib.urlopen(args[0])
         if text.info().gettype() == 'text/html':
-            print text.read()
+            years = re.findall(r'\d+\sto\s\d+</h2><ul>(.*?)</ul>', text.read())
+            for year in years:
+                print year
+            
+            
+            
     except IOError:
-        print "Could not access we address ", url
+        print "Could not access web address "
         
-        
-wget('http://www.bounty.com/pregnancy-and-birth/baby-names/top-baby-names')        
+    
+if __name__ == '__main__':
+  main()
